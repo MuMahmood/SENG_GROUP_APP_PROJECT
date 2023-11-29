83c38f641f7791cbacf9aa023365735d514b291e
Af02fA <affanmahmood2002@gmail.com>
2023-11-29 17:19:36 -0500

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SENG_GROUP_APP_PROJECT"
+include(":app")
