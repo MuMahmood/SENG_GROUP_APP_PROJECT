59665fd0a4be5322f076f927ed67122fea5accf0
Af02fA <affanmahmood2002@gmail.com>
2023-11-30 15:32:48 -0500

    Login Page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.seng_group_app_project"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.seng_group_app_project"
